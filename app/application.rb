4c2d6f8f4fcffe4e4fa12b16b78a502f9fdc8f63
Barbara Washington <barbara.m.washington@gmail.com>
2018-07-15 22:37:01 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,6 +6,11 @@ class Application
       resp = Rack::Response.new
       req = Rack::Request.new(env)
 
+      if req.path.match(/items/)
+        item_name = req.path.split("/items/").last
+
+        item = @@items.find{|i| i.name == item_name}
+
     end
 
 end
