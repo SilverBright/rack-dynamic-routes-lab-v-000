92a37b0425f96be4ba444290b7a8aa07f2882bf2
Barbara Washington <barbara.m.washington@gmail.com>
2018-07-15 22:38:31 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,18 @@ class Application
         item_name = req.path.split("/items/").last
 
         item = @@items.find{|i| i.name == item_name}
-      end
-    end
 
+        if item.nil?
+          resp.write "Item not found"
+          resp.status = 400
+        else
+          resp.write item.price
+        end
+      else
+        resp.write "Route not found"
+        resp.status = 404
+      end
+
+    resp.finish
+  end
 end
